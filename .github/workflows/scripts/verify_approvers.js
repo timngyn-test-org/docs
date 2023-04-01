886b979fa6505b2c0fdd0a0bb68a767774f3371f
Timothy Nguyen <timngyn@gmail.com>
2023-04-01 14:13:07 -0700

    test

--- .github/workflows/scripts/verify_approvers.js
+++ .github/workflows/scripts/verify_approvers.js
@@ -2,7 +2,7 @@ module.exports = {
   verifyApprovers: async ({ github, context }) => {
     // Listens for review submissions to update a comment on PR to keep track of who has actually approved
     // against who was originally requested
-    console.log(context);
+    // console.log(context);
 
     const {
       payload: {
@@ -28,38 +28,40 @@ module.exports = {
       context.payload.pull_request.requested_teams
     );
 
-    // const trackApprovers = octokit
-    //   .paginate(
-    //     'GET /repos/{owner}/{repo}/issues/{issue_number}/comments',
-    //     { owner: ownerLogin, repo: name, issue_number: number },
-    //     (response) =>
-    //       response.data.filter(
-    //         (comment) =>
-    //           comment.user.login === 'github-actions[bot]' &&
-    //           comment.body.startsWith('From Verify Approvals workflow')
-    //       )
-    //   )
-    //   .then((comment) => {
-    //     console.log('found comment!!!', comment);
+    // Try to find the comment by the `github-actions[bot]` so we can
+    // keep track of who the original list of requested reviewers were.
+    // Need to do this because Github API only shows who is currently requested.
+    // When a user submits their review (comment, approve, request changes), they are removed
+    // from the list of requested reviewers.
+    const prComments = await octokit.paginate(
+      rest.issues.listComments,
+      { owner: ownerLogin, repo: name, issue_number: number },
+      (response) =>
+        response.data.filter(
+          (comment) =>
+            comment.user.login === 'github-actions[bot]' &&
+            comment.body.startsWith('From Verify Approvals workflow')
+        )
+    );
 
-    //     return comment;
-    //   });
+    console.log('PR comments from the github bot: ', prComments);
 
-    const reviews = await github.rest.pulls.listReviews({
-      owner: ownerLogin,
-      repo: name,
-      pull_number: number
-    });
+    const reviews = await github.paginate(
+      github.rest.pulls.listReviews,
+      {
+        owner: ownerLogin,
+        repo: name,
+        pull_number: number
+      },
+      (response) =>
+        response.data.map((review) => ({
+          userLogin: review.user.login,
+          state: review.state,
+          submitted_at: review.submitted_at
+        }))
+    );
 
-    const reviews2 = await github.paginate(github.rest.pulls.listReviews, {
-      owner: ownerLogin,
-      repo: name,
-      pull_number: number
-    }, (response) => response.data.map((review) => ({ userLogin: review.user.login, state: review.state, submitted_at: review.submitted_at })));
-
-    console.log('listReviews: ', reviews.data);
-
-    console.log('paginate listreviews', reviews2);
+    console.log('List of reviews on PR: ', reviews);
 
     // const trackRequestedReviewsComment = `From Verify Approvals workflow
 
