f3de03330aff52b4833fbe428989280f384bb555
Timothy Nguyen <timngyn@gmail.com>
2023-04-22 01:02:32 -0700

    test redirects fix

---
+++ .github/workflows/scripts/check_for_redirects.js
@@ -0,0 +1,20 @@
+module.exports = {
+  getDeletedFiles: async ({ github, context, core }) => {
+    const {
+      issue: { number: issue_number },
+      repo: { owner, repo }
+    } = context;
+
+    const deletedFiles = await github.paginate(
+      'GET /repos/{owner}/{repo}/pulls/{pull_number}/files',
+      { owner, repo, pull_number: issue_number },
+      (response) => response.data.filter((file) => file.status === 'deleted')
+    );
+
+    console.log('Deleted file count: ', deletedFiles.length);
+    console.log('Deleted files: ', deletedFiles);
+
+    return deletedFiles.length;
+  },
+  addRedirectsLabel: 
+};
