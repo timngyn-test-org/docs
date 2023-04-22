28cb6cef75e9ea68dba6a67d84bccdbb661d2d3f
Timothy Nguyen <timngyn@gmail.com>
2023-04-22 01:22:58 -0700

    use 'removed'

--- .github/workflows/scripts/check_for_redirects.js
+++ .github/workflows/scripts/check_for_redirects.js
@@ -8,7 +8,7 @@ module.exports = {
     const deletedFiles = await github.paginate(
       'GET /repos/{owner}/{repo}/pulls/{pull_number}/files',
       { owner, repo, pull_number: issue_number },
-      (response) => response.data.filter((file) => file.status === 'deleted')
+      (response) => response.data.filter((file) => file.status === 'removed')
     );
 
     console.log('Deleted file count: ', deletedFiles.length);
